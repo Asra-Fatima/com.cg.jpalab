93d361c6311110db562048ca34fe0aa8299753b6
Asra-Fatima <fatima2000asra@gmail.com>
2021-11-11 22:05:41 +0530

    Lab2
    jpa lab2

---
+++ services/AuthorRepository.java
@@ -0,0 +1,36 @@
+package com.cg.jpalab.services;
+
+import com.cg.jpalab.services.Author;
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+public class AuthorRepository {
+    private EntityManager entityManager;
+    public AuthorRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    public Optional<Author> findById(Integer id) {
+        Author author = entityManager.find(Author.class, id);
+        return author != null ? Optional.of(author) : Optional.empty();
+    }
+    public List<com.cg.jpalab.services.Author> findAll() {
+        return entityManager.createQuery("from Author").getResultList();
+    }
+    public Optional<com.cg.jpalab.services.Author> findByName(String name) {
+        Author author = entityManager.createNamedQuery("Author.findByName", Author.class)
+                .setParameter("name", name)
+                .getSingleResult();
+        return author != null ? Optional.of(author) : Optional.empty();
+    }
+    public Optional<com.cg.jpalab.services.Author> save(com.cg.jpalab.services.Author author) {
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(author);
+            entityManager.getTransaction().commit();
+            return Optional.of(author);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+}
